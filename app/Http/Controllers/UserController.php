4defd4dd337cdb3b99f301e0ab0249a4b03daf2a
Gaco Razan Kamil <2241720091@student.polinema.ac.id>
2024-03-11 12:45:11 +0700

    Praktikum 2.5 - Pertemuan 4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -44,13 +44,56 @@ class UserController extends Controller
         //     ]
         // );
 
-        $user = UserModel::firstOrNew ([
-            'username' => 'manager33',
-            'nama' => 'Manager Tiga TIga',
+        // $user = UserModel::firstOrNew ([
+        //     'username' => 'manager33',
+        //     'nama' => 'Manager Tiga TIga',
+        //     'password' => Hash::make('12345'),
+        //     'level_id' => 2
+        // ]);
+        // $user->save();
+
+        // $user = UserModel::create([
+        //     'username' => 'manager55',
+        //     'nama' => 'Manager55',
+        //     'password' => Hash::make('12345'),
+        //     'level_id' => 2
+        // ]);
+
+        // $user -> username = 'manager56';
+
+        // $user->isDirty();
+        // $user->isDirty('username');
+        // $user->isDirty('nama');
+        // $user->isDirty(['nama', 'username']);
+
+        // $user->isClean();
+        // $user->isClean('username');
+        // $user->isClean('nama');
+        // $user->isClean(['username', 'nama']);
+
+        // $user->save();
+
+        // $user->isDirty();
+        // $user->isClean();
+
+        // dd($user->isDirty());
+
+        $user = UserModel::create([
+            'username' => 'manager11',
+            'nama' => 'Manager11',
             'password' => Hash::make('12345'),
             'level_id' => 2
         ]);
+
+        $user -> username = 'manager12';
+
         $user->save();
+
+        $user->wasChanged();
+        $user->wasChanged('username');
+        $user->wasChanged(['username', 'level_id']);
+        $user->wasChanged('nama');
+        dd($user->wasChanged(['nama', 'username']));
 
         return view('user.index', ['data' => $user]);
     }
